28d8e4b325219b778e913ee72034e0db36a32c45
Eugenio <eugenio@schinina.it>
2011-09-19 15:13:12 +0200

    Empty Eclipse project

---
+++ src/it/bluesoft/funapps/ItsFridayActivity.java
@@ -0,0 +1,13 @@
+package it.bluesoft.funapps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ItsFridayActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
